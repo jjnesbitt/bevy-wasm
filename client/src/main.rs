37091773d4032f0ca88b876c7d751459c27cda56
Jacob Nesbitt <jjnesbitt2@gmail.com>
2023-12-10 20:56:30 -0500

    Add background and camera following

--- client/src/main.rs
+++ client/src/main.rs
@@ -17,9 +17,13 @@ use shared::GameClient;
 const PLAYER_SIZE: Vec3 = Vec3::new(120.0, 120.0, 0.0);
 const GAP_BETWEEN_PLAYER_AND_FLOOR: f32 = 60.0;
 const PLAYER_SPEED: f32 = 500.0;
-
-const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
 const PLAYER_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
+
+// Map constants
+const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
+const MAP_SIZE: u32 = 5000;
+const GRID_WIDTH: f32 = 1.0;
+const UNITS_BETWEEN_LINES: f32 = 100.0;
 
 #[wasm_bindgen(module = "/js/foo.js")]
 extern "C" {
@@ -39,7 +43,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_event::<CollisionEvent>()
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup, setup_map))
         // Add our gameplay simulation systems to the fixed timestep schedule
         // which runs at 64 Hz by default
         .add_systems(
@@ -53,7 +57,10 @@ fn main() {
                 // `chain`ing systems together runs them in order
                 .chain(),
         )
-        .add_systems(Update, (read_gamepads, bevy::window::close_on_esc))
+        .add_systems(
+            Update,
+            (read_gamepads, handle_zoom, bevy::window::close_on_esc),
+        )
         .run();
 }
 
@@ -97,7 +104,7 @@ fn setup(mut commands: Commands) {
     commands.spawn((
         SpriteBundle {
             transform: Transform {
-                translation: Vec3::new(0.0, player_y, 0.0),
+                translation: Vec3::new(0.0, player_y, 50.0),
                 scale: PLAYER_SIZE,
                 ..default()
             },
@@ -110,6 +117,42 @@ fn setup(mut commands: Commands) {
         Player,
         Collider,
     ));
+}
+
+fn setup_map(mut commands: Commands) {
+    // Horizontal lines
+    for i in 0..=(MAP_SIZE / UNITS_BETWEEN_LINES as u32) {
+        commands.spawn(SpriteBundle {
+            transform: Transform::from_translation(Vec3::new(
+                0.,
+                ((i as f32) * UNITS_BETWEEN_LINES) - MAP_SIZE as f32 / 2.,
+                0.,
+            )),
+            sprite: Sprite {
+                color: Color::rgb(0.27, 0.27, 0.27),
+                custom_size: Some(Vec2::new(MAP_SIZE as f32, GRID_WIDTH)),
+                ..default()
+            },
+            ..default()
+        });
+    }
+
+    // Vertical lines
+    for i in 0..=(MAP_SIZE / UNITS_BETWEEN_LINES as u32) {
+        commands.spawn(SpriteBundle {
+            transform: Transform::from_translation(Vec3::new(
+                ((i as f32) * UNITS_BETWEEN_LINES) - MAP_SIZE as f32 / 2.,
+                0.,
+                0.,
+            )),
+            sprite: Sprite {
+                color: Color::rgb(0.27, 0.27, 0.27),
+                custom_size: Some(Vec2::new(GRID_WIDTH, MAP_SIZE as f32)),
+                ..default()
+            },
+            ..default()
+        });
+    }
 }
 
 fn read_gamepads(
@@ -146,6 +189,7 @@ fn read_gamepads(
 fn move_player(
     keyboard_input: Res<Input<KeyCode>>,
     mut query: Query<&mut Transform, With<Player>>,
+    mut cameras: Query<&mut Transform, (With<Camera>, Without<Player>)>,
     time: Res<Time>,
 ) {
     let mut player_transform = query.single_mut();
@@ -170,6 +214,55 @@ fn move_player(
         player_transform.translation.x + x_direction * PLAYER_SPEED * time.delta_seconds();
     player_transform.translation.y =
         player_transform.translation.y + y_direction * PLAYER_SPEED * time.delta_seconds();
+
+    // Set camera center to match player's
+    for mut transform in &mut cameras {
+        transform.translation.x = player_transform.translation.x;
+        transform.translation.y = player_transform.translation.y;
+    }
+}
+
+fn handle_zoom(
+    gamepads: Res<Gamepads>,
+    keyboard_input: Res<Input<KeyCode>>,
+    gamepad_input: Res<Input<GamepadButton>>,
+    mut proj_query: Query<&mut OrthographicProjection, With<Camera>>,
+    time: Res<Time>,
+) {
+    for mut projection in proj_query.iter_mut() {
+        let scale_amt = 1.5;
+        let mut log_scale = projection.scale.ln();
+
+        // Keyboard input
+        if keyboard_input.pressed(KeyCode::PageUp) {
+            log_scale -= scale_amt * time.delta_seconds();
+        }
+        if keyboard_input.pressed(KeyCode::PageDown) {
+            log_scale += scale_amt * time.delta_seconds();
+        }
+
+        // Gamepad input
+        if let Some(gamepad) = gamepads.iter().next() {
+            let left_trigger = GamepadButton {
+                gamepad,
+                button_type: GamepadButtonType::LeftTrigger2,
+            };
+            if gamepad_input.pressed(left_trigger) {
+                log_scale += scale_amt * time.delta_seconds();
+            }
+
+            let right_trigger = GamepadButton {
+                gamepad,
+                button_type: GamepadButtonType::RightTrigger2,
+            };
+            if gamepad_input.pressed(right_trigger) {
+                log_scale -= scale_amt * time.delta_seconds();
+            }
+        }
+
+        // Set new value
+        projection.scale = log_scale.exp();
+    }
 }
 
 fn send_player_position(query: Query<&Transform, With<Player>>) {
@@ -198,16 +291,13 @@ fn read_web_socket(mut commands: Commands, query: Query<(Entity, &OtherPlayer)>)
 
     // Create an entity for each other player found
     if let Some(clients) = serde_json::from_str::<Vec<GameClient>>(&msg).ok() {
-        // console_log(&format!("{:?}", clients));
-
         for client in clients.iter() {
-            console_log(&client.uuid.to_string()[0..4].to_string());
-
             commands
                 .spawn((
                     SpriteBundle {
                         transform: Transform {
-                            translation: Vec3::new(client.position[0], client.position[1], 0.0),
+                            // Position player forward, in-front of the background
+                            translation: Vec3::new(client.position[0], client.position[1], 50.0),
                             ..default()
                         },
                         sprite: Sprite {
